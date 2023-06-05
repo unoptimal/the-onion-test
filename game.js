84ae6a98d3c22f367a3db9364b1dbce2a610c8e9
unoptimal <unoptimalblogs@gmail.com>
2023-06-04 23:00:12 -0400

    functional but needs further styling

--- game.js
+++ game.js
@@ -4,9 +4,10 @@ window.onload = function() {
     let currentSource = null;
     let currentSourceUrl = null;
     let score = 0;
-    let rounds = 0;
+    let rounds = -1;
     let confidence = 50;
-    
+    let guess = null;
+
     fetch('TheOnion_titles.json')
         .then(response => response.json())
         .then(data => {
@@ -18,72 +19,98 @@ window.onload = function() {
                     nextHeadline();
                 });
         });
-    
-    function scoreCalculation(confidence, isCorrect) {
-        let correctScores = {50: 0, 60: 26, 70: 49, 80: 68, 90: 85, 99: 99};
-        let incorrectScores = {50: 0, 60: -32, 70: -74, 80: -132, 90: -232, 99: -564};
-        if (isCorrect) {
-            return correctScores[confidence];
-        } else {
-            return incorrectScores[confidence];
-        }
+
+    document.getElementById('startButton').addEventListener('click', function() {
+        document.getElementById('intro').style.display = 'none';
+        document.getElementById('outerGameContainer').style.display = 'block';
+        document.getElementById('gameContainer').style.display = 'block';
+        nextHeadline();
+    });
+
+    const confidenceButtons = document.getElementsByClassName('confidenceButton');
+    for (let i = 0; i < confidenceButtons.length; i++) {
+        confidenceButtons[i].addEventListener('click', function() {
+            confidence = parseInt(this.value);
+            this.style.backgroundColor = '#CCCCCC';
+            processGuess(guess === currentSource);
+            for(let j = 0; j < confidenceButtons.length; j++) {
+                confidenceButtons[j].disabled = true;
+            }
+        });
     }
-    
+
+    document.getElementById('onionButton').addEventListener('click', function() {
+        guess = 'The Onion';
+        this.style.backgroundColor = '#CCCCCC';
+        document.getElementById('notOnionButton').disabled = true;
+        document.getElementById('sure').style.display = 'block';
+        document.getElementById('confidence-wrapper').style.display = 'flex';
+    });
+
+    document.getElementById('notOnionButton').addEventListener('click', function() {
+        guess = 'Not The Onion';
+        this.style.backgroundColor = '#CCCCCC';
+        document.getElementById('onionButton').disabled = true;
+        document.getElementById('sure').style.display = 'block';
+        document.getElementById('confidence-wrapper').style.display = 'flex';
+    });
+
+    function processGuess(isCorrect) {
+        score += scoreCalculation(isCorrect);
+        document.getElementById('source').textContent = 'This was: ' + currentSource;
+        document.getElementById('score').textContent = score;
+        document.getElementById('continueButton').style.display = 'block';
+
+
+    }
+
     function nextHeadline() {
         if (headlines.length > 0 && rounds < 7) {
             let index = Math.floor(Math.random() * headlines.length);
             currentHeadline = headlines[index].headline;
             currentSource = headlines[index].source;
             currentSourceUrl = headlines[index].sourceUrl;
-            headlines.splice(index, 1); 
+            headlines.splice(index, 1);
             document.getElementById('headline').textContent = currentHeadline;
-            document.getElementById('source').textContent = ''; 
-            document.getElementById('sourceLink').style.display = 'none'; 
-            document.getElementById('continueButton').style.display = 'none'; 
+            document.getElementById('source').textContent = '';
+            document.getElementById('sure').style.display = 'none';
+            document.getElementById('continueButton').style.display = 'none';
             rounds++;
-            document.getElementById('rounds').textContent = 'Round: ' + rounds + '/7';
-        } else {
-            document.getElementById('headline').textContent = 'Game over!';
-            document.getElementById('score').textContent = score;
+            document.getElementById('rounds').textContent = 'Round ' + rounds + ' of 7';
 
-            document.getElementById('sourceLink').style.display = 'none'; 
+            document.getElementById('onionButton').style.display = 'inline-block';
+            document.getElementById('onionButton').style.backgroundColor = '';
+            document.getElementById('onionButton').disabled = false;
+            document.getElementById('notOnionButton').style.display = 'inline-block';
+            document.getElementById('notOnionButton').style.backgroundColor = '';
+            document.getElementById('notOnionButton').disabled = false;
+            document.getElementById('confidence-wrapper').style.display = 'none';
+
+            for(let j = 0; j < confidenceButtons.length; j++) {
+                confidenceButtons[j].style.backgroundColor = '';
+                confidenceButtons[j].disabled = false;
+            }
+        } else {
+            document.getElementById('score').textContent = score;
+            document.getElementById('headline').style.display = 'none';
+            document.getElementById('sure').style.display = 'none';
             document.getElementById('rounds').textContent = '';
             document.getElementById('source').textContent = '';
             document.getElementById('onionButton').style.display = 'none';
             document.getElementById('notOnionButton').style.display = 'none';
             document.getElementById('continueButton').style.display = 'none';
             document.getElementById('confidence-wrapper').style.display = 'none';
+            document.getElementById('gameContainer').style.display = 'none';
         }
     }
-    
-    function processGuess(isCorrect) {
-        score += scoreCalculation(confidence, isCorrect);
-        document.getElementById('source').textContent = 'This was: ' + currentSource;
-        document.getElementById('score').textContent = score;
-        document.getElementById('continueButton').style.display = 'block'; 
-    
-        document.getElementById('onionButton').disabled = true;
-        document.getElementById('notOnionButton').disabled = true;
-    
-        document.getElementById('sourceLink').href = currentSourceUrl;
-        document.getElementById('sourceLink').style.display = 'block';
+
+    function scoreCalculation(isCorrect) {
+        let correctScores = {50: 0, 60: 26, 70: 49, 80: 68, 90: 85, 99: 99};
+        let incorrectScores = {50: 0, 60: -32, 70: -74, 80: -132, 90: -232, 99: -564};
+        return isCorrect ? correctScores[confidence] : incorrectScores[confidence];
     }
-    
-    document.getElementById('onionButton').addEventListener('click', function() {
-        processGuess(currentSource === 'The Onion');
-    });
-    
-    document.getElementById('notOnionButton').addEventListener('click', function() {
-        processGuess(currentSource === 'Not The Onion');
-    });
-    
+
     document.getElementById('continueButton').addEventListener('click', function() {
-        document.getElementById('onionButton').disabled = false;
-        document.getElementById('notOnionButton').disabled = false;
         nextHeadline();
-    });
-    
-    document.getElementById('confidence').addEventListener('change', function(event) {
-        confidence = parseInt(event.target.value);
     });
 }
